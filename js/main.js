3060ab17cc514540a3bbb358026012ff20e60d3f
marisabivi <biv.marisa@yahoo.com>
2016-04-29 10:34:45 -0400

    work content

--- js/main.js
+++ js/main.js
@@ -1,6 +1,72 @@
 var hamburgerToggle = $('#toggle-nav');
 var sideNav = $ ('.side-nav');
+var $btnShowHide = $('.showhide');
 
+//lightbox
+function resettoggle() {
+	var e = document.getElementById(id);
+		e.style.display = 'none';
+		}
+
+function toggle_visibility(id) {
+	var e = document.getElementById(id);
+		if(e.style.display == 'none')
+			e.style.display = 'block';
+
+	else
+			e.style.display = 'none';
+		}
+
+		//prevents IE error??
+		function toggle_visibility(id) {
+			var e = document.getElementById(id);
+			if(e != null) {
+				if(e.style.display == 'none')
+				{ e.style.display = 'block'; }
+				else { e.style.display = 'none'; }
+			}
+		}
+
+
+	function hide(id) {
+			var e = document.getElementById(id);
+			if(e.style.display == 'block')
+				e.style.display = 'none';
+			}
+
+//smoothscroll
+//$('.fixednav').localScroll({
+  //target:'body'
+//});
+
+var hashTagActive = "";
+$(document).ready( function() {
+	$(".smooth-scroll a").click(function (event) {
+
+		if(hashTagActive != this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
+			event.preventDefault();
+
+			//calculate destination place
+			var dest = 0;
+			if ( $(this.hash).offset().top > $(document).height() - $(window).height() ) {
+				dest = $(document).height() - $(window).height();
+			}
+			else {
+				dest = $(this.hash).offset().top;
+			}
+
+			//go to destination
+			$('html, body').animate({
+			    scrollTop: dest
+			}, 700, 'swing');
+
+			hashTagActive = this.hash;
+		}
+
+	});
+});
+
+// Hamburger Toggle Animation
 
 var sideNavClickHandler = function (e) {
 	var isActive = sideNav.attr('data-state');
@@ -24,8 +90,6 @@ var sideNavItemClick = function () {
 		sideNav.attr('data-state','active');
 	}
 };
-
-// Hamburger Toggle Animation
 
 $( "#toggle-nav" ).on( "click", function() {
   $( this ).toggleClass( "active" );
---
+++ js/smooth-scrolling.js
@@ -0,0 +1,4 @@
+
+$('.fixednav').localScroll({
+    target:'body'
+});
